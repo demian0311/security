e6becbe3a3830f2fd07dbb90099e712263bf85d7
Demian Neidetcher <demian0311@gmail.com>
2013-09-08 13:31:59 -0500

    refactor after getting it to work

--- scend/src/main/scala/com/neidetcher/security/Chapter02.scala
+++ scend/src/main/scala/com/neidetcher/security/Chapter02.scala
@@ -5,40 +5,38 @@ import javax.crypto.Cipher
 import java.security.Security
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 
+/**
+ * This uses a symmetric key, hard-coded in the class.
+ */
 class Chapter02 {
 
-  def doit(){
-    Security.addProvider(new BouncyCastleProvider())
+  Security.addProvider(new BouncyCastleProvider())
 
-    val input: Array[Byte] = Array[Byte](
-      0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-      0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77)
+  val keyBytes: Array[Byte] = Array[Byte](
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17)
 
-    val keyBytes: Array[Byte] = Array[Byte](
-      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-      0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-      0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17)
+  val key = new SecretKeySpec(keyBytes, "AES")
+  val cipher: Cipher = Cipher.getInstance("AES/ECB/NoPadding", "BC")
 
-    println("keyBytes: " + keyBytes)
-    println("keyBytes length: " + keyBytes.length)
-
-    val key = new SecretKeySpec(keyBytes, "AES")
-    val cipher: Cipher = Cipher.getInstance("AES/ECB/NoPadding", "BC")
-
-    println("input size: " + input.length)
-    println("input text: " + Util.toHex(input))
-
-    // encryption pass
+  def encrypt(input: Array[Byte]): Array[Byte] = {
     cipher.init(Cipher.ENCRYPT_MODE, key)
-    val cipherText: Array[Byte] = new Array[Byte](input.length)
-    val ctLength = cipher.update(input, 0, input.length, cipherText, 0)
-    println("cipher text: " + Util.toHex(cipherText) + " bytes: " + ctLength)
 
-    // decryption pass
+    val cipherText: Array[Byte] = new Array[Byte](input.length)
+    cipher.update(input, 0, input.length, cipherText, 0)
+    cipher.doFinal(cipherText, input.length)
+
+    cipherText
+  }
+
+  def decrypt(encryptedBytes: Array[Byte]): Array[Byte] = {
     cipher.init(Cipher.DECRYPT_MODE, key)
-    val plainText: Array[Byte] = new Array[Byte](ctLength)
-    var ptLength: Int = cipher.update(cipherText, 0, ctLength, plainText, 0)
-    ptLength = ptLength + cipher.doFinal(plainText, ptLength)
-    println("plain text: " + Util.toHex(plainText) + " bypes: " + ptLength)
+
+    val plainText: Array[Byte] = new Array[Byte](encryptedBytes.length)
+    cipher.update(encryptedBytes, 0, encryptedBytes.length, plainText, 0)
+    cipher.doFinal(plainText, encryptedBytes.length)
+
+    plainText
   }
 }
--- scend/src/test/scala/com/neidetcher/security/Chapter02Test.scala
+++ scend/src/test/scala/com/neidetcher/security/Chapter02Test.scala
@@ -1,14 +1,29 @@
 package com.neidetcher.security
 
 import org.junit.Test
+import org.junit.Assert._
 
 class Chapter02Test {
 
   val chapter02 = new Chapter02()
 
   @Test def testDoit(){
-    chapter02.doit()
-    println("hello")
+    val plainBytes: Array[Byte] = Array[Byte](
+      0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+      0x88.toByte, 0x99.toByte, 0xaa.toByte, 0xbb.toByte,
+      0xcc.toByte, 0xdd.toByte, 0xee.toByte, 0xff.toByte
+    )
+
+    println("PLN: " + Util.toHex(plainBytes))
+    val encryptedBytes:Array[Byte] = chapter02.encrypt(plainBytes)
+    println("ENC: " + Util.toHex(encryptedBytes))
+
+    assertNotEquals(Util.toHex(plainBytes), Util.toHex(encryptedBytes))
+
+    val decryptedBytes = chapter02.decrypt(encryptedBytes)
+    println("DEC: " + Util.toHex(decryptedBytes))
+
+    assertEquals(Util.toHex(plainBytes), Util.toHex(decryptedBytes))
   }
 
 }
