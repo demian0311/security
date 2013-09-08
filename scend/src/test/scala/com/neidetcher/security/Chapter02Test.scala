db17e6eceb336579f10ccd372677c1c2549579a2
Demian Neidetcher <demian0311@gmail.com>
2013-09-08 11:40:37 -0500

    chapter 2 test

---
+++ scend/src/test/scala/com/neidetcher/security/Chapter02Test.scala
@@ -0,0 +1,14 @@
+package com.neidetcher.security
+
+import org.junit.Test
+
+class Chapter02Test {
+
+  val chapter02 = new Chapter02()
+
+  @Test def testDoit(){
+    chapter02.doit()
+    println("hello")
+  }
+
+}
