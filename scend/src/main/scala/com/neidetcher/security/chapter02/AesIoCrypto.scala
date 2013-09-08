901595f845fdf1ce5c569e6cb5f4b4cacf510586
Demian Neidetcher <demian0311@gmail.com>
2013-09-08 18:39:04 -0500

    organizing

--- scend/src/main/scala/com/neidetcher/security/chapter02/AesIoCrypto.scala
+++ scend/src/main/scala/com/neidetcher/security/chapter02/AesIoCrypto.scala
@@ -2,7 +2,6 @@ package com.neidetcher.security.chapter02
 
 import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
 import javax.crypto.{CipherOutputStream, CipherInputStream, Cipher}
-import com.neidetcher.security.Util
 import java.security.Security
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.io.{ByteArrayOutputStream, ByteArrayInputStream}
@@ -11,21 +10,31 @@ class AesIoCrypto {
 
   Security.addProvider(new BouncyCastleProvider())
 
-  val keyBytes = Array[Byte](
-    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17)
+  /**
+    * I don't know how expensive this operation is but it prevents
+    * us from having a shared Cipher that has different MODEs
+    * set on it.
+    */
+  def getCipher(mode: Int): Cipher = {
 
-  val ivBytes = Array[Byte](
-    0x00, 0x01, 0x02, 0x03, 0x00, 0x01, 0x02, 0x03,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01)
+    val keyBytes = Array[Byte](
+      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+      0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+      0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17)
+    val key = new SecretKeySpec(keyBytes, "AES")
 
-  val key = new SecretKeySpec(keyBytes, "AES")
-  val ivParameterSpec = new IvParameterSpec(ivBytes)
-  val cipher = Cipher.getInstance("AES/CTR/NoPadding", "BC")
+    val ivBytes = Array[Byte](
+      0x00, 0x01, 0x02, 0x03, 0x00, 0x01, 0x02, 0x03,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01)
+    val ivParameterSpec = new IvParameterSpec(ivBytes)
+
+    val cipher = Cipher.getInstance("AES/CTR/NoPadding", "BC")
+    cipher.init(mode, key, ivParameterSpec)
+    cipher
+  }
 
   def encrypt(input: Array[Byte]): Option[Array[Byte]] = {
-    cipher.init(Cipher.ENCRYPT_MODE, key, ivParameterSpec)
+    val cipher = getCipher(Cipher.ENCRYPT_MODE)
 
     val byteArrayInputStream = new ByteArrayInputStream(input)
     val cipherInputStream = new CipherInputStream(byteArrayInputStream, cipher)
@@ -44,8 +53,7 @@ class AesIoCrypto {
   }
 
   def decrypt(encryptedByteArray: Array[Byte]): Option[Array[Byte]] = {
-    // we share some state here, this is not safe
-    cipher.init(Cipher.DECRYPT_MODE, key, ivParameterSpec)
+    val cipher = getCipher(Cipher.DECRYPT_MODE)
 
     val byteArrayOutputStream = new ByteArrayOutputStream()
     val cipherOutputStream = new CipherOutputStream(byteArrayOutputStream, cipher)
